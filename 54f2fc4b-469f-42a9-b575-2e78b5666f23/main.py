dd4ee7597c11449ee8e51b2e5b78e73b14792431
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-01-15 21:40:27 +0500

    User Quiver Quantitative saved changes to multiple files files at 01/15/2026, 16:40:26 UTC time.

--- 54f2fc4b-469f-42a9-b575-2e78b5666f23/main.py
+++ 54f2fc4b-469f-42a9-b575-2e78b5666f23/main.py
@@ -1,1 +1,36 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import RobBresnahan
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [RobBresnahan()]
+        self.tickers = []
+        self.allocations = {}
+        self.count = 0
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        rob_bresnahan_holdings = data[("rob_bresnahan",)]
+        
+        if rob_bresnahan_holdings:
+            alloc_dict = rob_bresnahan_holdings[-1]['allocations']
+            log(f"Trading: {rob_bresnahan_holdings[-1]['allocations']}")
+            self.allocations = alloc_dict
+            self.count = 1
+        else:
+            if self.count < 1:
+                self.allocations = {"SPY": 1}
+        log(f"allocations:{self.allocations}")
+        return TargetAllocation(self.allocations)
